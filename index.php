55ea9b963e9f9edccef4c861736f73cbc70e6f2d
tomdemort4 <tomeu766@gmail.com>
2017-10-09 20:17:56 +0200

    acualizada hoja de estilos. Actualizado el bc de las paginas (#8)
    .

--- index.php
+++ index.php
@@ -15,10 +15,9 @@ $noticias = $queryBuilder->getTableNoticias();
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" 
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-
+        <link rel="stylesheet" href="resources/css/styles.css">
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-        <link rel="stylesheet" href="resources/css/styles.css">
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
--- nuestrasActividades.php
+++ nuestrasActividades.php
@@ -11,15 +11,18 @@ include 'core/Factory.php';
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Actividades</title>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-                integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-        crossorigin="anonymous"></script>
+                integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
-        <!-- Compiled and minified JavaScript -->
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
         <link rel="stylesheet" href="resources/css/styles.css">
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
     </head>
+   <script>
+
+   console.log("hola");
+   </script>
     <body>
         <div class="wrapper">
             <?php require './shared/navbar.php'; ?>
